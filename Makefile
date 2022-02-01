8ff0998e33fd03f4e0f7f503b10f595fd79a42ee
Heng Li <lh3@me.com>
2022-02-01 17:38:56 -0500

    added edlib

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
 CC=			gcc
+CXX=		g++
 CFLAGS=		-g -Wall -O2
+CXXFLAGS=	$(CFLAGS)
 CPPFLAGS=
 INCLUDES=
-OBJS=		lv89.o
-PROG=		test-ed
+OBJS=		lv89.o edlib.o
+PROG=		ed-test
 LIBS=		-lz -lpthread -lm
 
 ifneq ($(asan),)
@@ -11,24 +13,28 @@ ifneq ($(asan),)
 	LIBS+=-fsanitize=address
 endif
 
-.SUFFIXES:.c .o
+.SUFFIXES:.c .cpp .o
 .PHONY:all clean depend
 
 .c.o:
 		$(CC) -c $(CFLAGS) $(CPPFLAGS) $(INCLUDES) $< -o $@
 
+.cpp.o:
+		$(CXX) -c $(CXXFLAGS) $(CPPFLAGS) $(INCLUDES) $< -o $@
+
 all:$(PROG)
 
 $(PROG):$(OBJS) main.o
-		$(CC) $(CFLAGS) $^ -o $@ $(LIBS)
+		$(CXX) $(CFLAGS) $^ -o $@ $(LIBS)
 
 clean:
 		rm -fr gmon.out *.o a.out $(PROG) *~ *.a *.dSYM
 
 depend:
-		(LC_ALL=C; export LC_ALL; makedepend -Y -- $(CFLAGS) $(DFLAGS) -- *.c)
+		(LC_ALL=C; export LC_ALL; makedepend -Y -- $(CFLAGS) $(DFLAGS) -- *.c *.cpp)
 
 # DO NOT DELETE
 
 lv89.o: lv89.h
 main.o: lv89.h ketopt.h kseq.h
+edlib.o: edlib.h
--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lv89.h"
+#include "edlib.h"
 #include "ketopt.h"
 #include "kseq.h"
 KSEQ_INIT(gzFile, gzread)
@@ -12,16 +13,17 @@ int main(int argc, char *argv[])
 	gzFile fp1, fp2;
 	kseq_t *ks1, *ks2;
 	ketopt_t o = KETOPT_INIT;
-	int c, s, is_global = 0;
-	uint8_t *mem;
+	int c, s, is_global = 0, use_edlib = 0;
 
-	while ((c = ketopt(&o, argc, argv, 1, "g", 0)) >= 0) {
+	while ((c = ketopt(&o, argc, argv, 1, "gl", 0)) >= 0) {
 		if (c == 'g') is_global = 1;
+		else if (c == 'l') use_edlib = 1;
 	}
 	if (argc - o.ind < 2) {
 		fprintf(stderr, "Usage: test-ed [options] <in1.fa> <in2.fa>\n");
 		fprintf(stderr, "Options:\n");
 		fprintf(stderr, "  -g    count gaps at the end of the target\n");
+		fprintf(stderr, "  -l    use edlib instead\n");
 		return 1;
 	}
 
@@ -33,9 +35,16 @@ int main(int argc, char *argv[])
 	kseq_read(ks1);
 	kseq_read(ks2);
 
-	mem = (uint8_t*)malloc((ks1->seq.l + ks2->seq.l) * 16);
-	s = lv_ed(ks1->seq.l, ks1->seq.s, ks2->seq.l, ks2->seq.s, is_global, mem);
-	free(mem);
+	if (use_edlib) {
+		EdlibAlignResult rst;
+		rst = edlibAlign(ks2->seq.s, ks2->seq.l, ks1->seq.s, ks1->seq.l,
+				edlibNewAlignConfig(-1, is_global? EDLIB_MODE_NW : EDLIB_MODE_SHW, EDLIB_TASK_DISTANCE, NULL, 0));
+		s = rst.editDistance;
+	} else {
+		uint8_t *mem = (uint8_t*)malloc((ks1->seq.l + ks2->seq.l) * 16);
+		s = lv_ed(ks1->seq.l, ks1->seq.s, ks2->seq.l, ks2->seq.s, is_global, mem);
+		free(mem);
+	}
 	printf("%s\t%s\t%d\n", ks1->name.s, ks2->name.s, s);
 
 	kseq_destroy(ks1);
