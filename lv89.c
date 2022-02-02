c922bc88ac780cb5898ff417cbf9aeb45caf745c
Heng Li <lh3@me.com>
2022-02-01 22:30:06 -0500

    added LICENSE

--- lv89.c
+++ lv89.c
@@ -1,5 +1,30 @@
+/*
+The MIT License
+
+Copyright (c) 2022-     Dana-Farber Cancer Institute
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include <stdint.h>
-#include <stdio.h>
 #include "lv89.h"
 
 typedef struct {
@@ -74,8 +99,7 @@ static int32_t wf_step(int32_t tl, const char *ts, int32_t ql, const char *qs, i
 int32_t lv_ed(int32_t tl, const char *ts, int32_t ql, const char *qs, int32_t is_global, uint8_t *mem)
 {
 	int32_t s = 0, n = 1;
-	wf_diag_t *a;
-	a = (wf_diag_t*)mem;
+	wf_diag_t *a = (wf_diag_t*)mem;
 	a[0].d = 0, a[0].k = -1;
 	while (1) {
 		n = wf_step(tl, ts, ql, qs, n, a, is_global);
