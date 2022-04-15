e2da101eb8d7d0c355ae7a10594fd76b2b7917e5
Tim Dunn <me.timd1@gmail.com>
2022-04-15 18:25:55 -0400

    Misc changes to conversion scripts
    - split VCF->FASTA into VCF->FASTA and VCF->SAM
    - BAM -> VCF now works with cigartuples
    - may need to update scripts to stay compatible with nPoRe not expanding cigar

--- scripts/bam_to_vcf.py
+++ scripts/bam_to_vcf.py
@@ -1,4 +1,6 @@
 import argparse, subprocess
+from time import perf_counter
+from collections import defaultdict
 
 from lib.vcf import *
 from lib.cig import *
@@ -9,28 +11,42 @@ def main():
     get_bam_regions()
     print([f"{ctg}:{start}-{stop}" for (ctg, start ,stop) in cfg.args.regions])
 
-    bam = pysam.AlignmentFile(cfg.args.bam, "rb")
+    print("> extracting read data")
+    try:
+        bam = pysam.AlignmentFile(cfg.args.bam, "rb")
+    except ValueError: 
+        print(f"ERROR: BAM file '{cfg.args.bam}' contains no data.")
+        exit(1)
+
+    ctg1 = hap1 = pos1 = seq1 = ref1 = cig = None
+    ctg2 = hap2 = pos2 = seq2 = ref2 = cig = None
     for ctg, beg, end in cfg.args.regions:
         for read in bam.fetch(ctg, beg, end):
             if read.query_name == "chr20-hap1":
                 ctg1 = read.reference_name
                 hap1 = 1
+                pos1 = read.reference_start
                 seq1 = read.query_alignment_sequence.upper()
                 ref1 = read.get_reference_sequence().upper()
-                cig1 = read.cigarstring
+                cig1 = read.cigartuples
             elif read.query_name == "chr20-hap2":
                 ctg2 = read.reference_name
                 hap2 = 2
+                pos2 = read.reference_start
                 seq2 = read.query_alignment_sequence.upper()
                 ref2 = read.get_reference_sequence().upper()
-                cig2 = read.cigarstring
+                cig2 = read.cigartuples
             else:
                 print("ERROR: unexpected haplotype.")
 
-    hap1_data = (ctg1, hap1, seq1, ref1, cig1)
-    hap2_data = (ctg2, hap2, seq2, ref2, cig2)
+    if ctg1 == None:
+        print("ERROR: haplotypes unaligned.")
+        exit(1)
 
-    print('\n> generating standardized vcfs')
+    hap1_data = [(ctg1, hap1, pos1, seq1, ref1, cig1)]
+    hap2_data = [(ctg2, hap2, pos2, seq2, ref2, cig2)]
+
+    print('> generating standardized vcfs')
     filename = cfg.args.bam[:-4]
     vcf1 = gen_vcf(hap1_data, 1, filename)
     vcf2 = gen_vcf(hap2_data, 2, filename)
--- scripts/vcf_to_sam.py
+++ scripts/vcf_to_sam.py
@@ -5,33 +5,20 @@ from lib.cig import *
 
 def main():
 
-    print("> getting regions")
     get_vcf_regions()
-    print(cfg.args.regions)
-
-    print("> splitting vcf")
     filename = cfg.args.vcf[:-7]
     vcf1, vcf2 = split_vcf(cfg.args.vcf, vcf_out_pre=filename)
-
-    print("> applying hap1")
     hap1_data = apply_vcf(vcf1, 1)
-
-    print("> applying hap2")
     hap2_data = apply_vcf(vcf2, 2)
-
-    print("> saving FASTA")
-    fasta_filename = f"{filename}.fasta"
-    first = True
-    with open(fasta_filename, "w") as fasta:
-        for (ctg1, hap1, seq1, ref1, cig1), (ctg2, hap2, seq2, ref2, cig2) in \
-                zip(hap1_data, hap2_data):
-            assert(ctg1 == ctg2)
-            n = '\n' if not first else ''
-            fasta.write(f"{n}>{ctg1}-hap{hap1}")
-            fasta.write(f"\n{seq1}")
-            fasta.write(f"\n>{ctg2}-hap{hap2}")
-            fasta.write(f"\n{seq2}")
-            first = False
+    print("@HD\tVN:1.6\tSO:coordinate", end="")
+    for ctg, start, stop in cfg.args.regions:
+        print(f"\n@SQ\tSN:{ctg}\tLN:{stop+1}", end ="")
+    print(f"\n@PG\tPN:vcfdist\tID:vcf_to_fasta\tVN:0.0.1\tCL:{' '.join(sys.argv)}", end ="")
+    for (ctg1, hap1, seq1, ref1, cig1), (ctg2, hap2, seq2, ref2, cig2) in \
+            zip(hap1_data, hap2_data):
+        assert(ctg1 == ctg2)
+        print(f"\n{ctg1}-hap{hap1}\t0\t{ctg1}\t1\t60\t{collapse_cigar(cig1)}\t*\t0\t{ref_len(cig1)}\t{seq1}\t{'*'*len(seq1)}", end ="")
+        print(f"\n{ctg2}-hap{hap2}\t0\t{ctg2}\t1\t60\t{collapse_cigar(cig2)}\t*\t0\t{ref_len(cig2)}\t{seq2}\t{'*'*len(seq2)}", end ="")
 
 
 
