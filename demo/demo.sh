e1ebbd217d2e76f0a80fc29dc125b1e72a5193f7
Tim Dunn <me.timd1@gmail.com>
2023-09-19 17:40:45 -0400

    Added PR plotting to demo
     - can be done with/without partial credit
     - this small example doesn't generate a very nice curve
     - done at a reviewer's request

--- demo/demo.sh
+++ demo/demo.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+echo -e "Running vcfdist..."
 ../src/vcfdist \
     query.vcf \
     nist-v4.2.1_chr1_5Mb.vcf.gz \
@@ -5,3 +7,8 @@
     -b nist-v4.2.1_chr1_5Mb.bed \
     -p results/ \
     -v 0
+echo -e "done!"
+
+echo -en "Plotting results..."
+python3 pr_plot.py
+echo -e "done!"
