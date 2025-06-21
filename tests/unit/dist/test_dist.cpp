26d7aa46afe00eb59fe620b39040672f66c77e4c
Tim Dunn <tjdunn18@gmail.com>
2025-06-21 13:19:44 -0400

    fix: replaced obsolete CIGAR score test with NG50 test

--- tests/unit/dist/test_dist.cpp
+++ tests/unit/dist/test_dist.cpp
@@ -4,10 +4,18 @@
 
 namespace {
 
-TEST(CigarScoreTest, BasicCigarScoreCalc) {
+TEST(TestNG50, TestNG50Calc) {
+    std::vector<int> phase_blocks1 = {25, 50};
+    EXPECT_EQ(50, calc_ng50(phase_blocks1, 100));
 
-    std::vector<int> cigar = {PTR_MAT, PTR_INS, PTR_INS, PTR_MAT};
-    EXPECT_EQ(7, calc_cig_swg_score(cigar, 3 /* sub */, 5 /* open */, 2 /* extend */));
+    std::vector<int> phase_blocks2 = {1, 25, 25, 50};
+    EXPECT_EQ(25, calc_ng50(phase_blocks2, 101));
+
+    std::vector<int> phase_blocks3 = {90};
+    EXPECT_EQ(90, calc_ng50(phase_blocks3, 100));
+
+    std::vector<int> phase_blocks4 = {1, 10, 20};
+    EXPECT_EQ(0, calc_ng50(phase_blocks4, 100));
 }
 
 } // namespace
